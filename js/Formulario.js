4040101fc0c7785823f49c7869d8f4ec771a3832
leandro goñi <leandrodavid13@hotmail.com>
2022-06-16 14:04:22 -0300

    textCamp

--- js/Formulario.js
+++ js/Formulario.js
@@ -4,28 +4,8 @@ $(document).ready(function () {
 	let formulario = $('#FormularioContacto');
 	// validaciones
 	let Correo = /^[a-z0-9]+(\.[_a-z0-9]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,15})$/
+	let Text = /^[a-zA-Z0-9\_\-]{4,16}$/
 
-	const expresiones = {
-		usuario: /^[a-zA-Z0-9\_\-]{4,16}$/, // Letras, numeros, guion y guion_bajo
-		nombre: /^[a-zA-ZÀ-ÿ\s]{1,40}$/, // Letras y espacios, pueden llevar acentos.
-		password: /^.{4,12}$/, // 4 a 12 digitos.
-		
-		telefono: /^\d{7,14}$/ // 7 a 14 numeros.
-	}
-
-	const ValidarForm = (e) => {
-		console.log(Correo.test(e.target.email))
-		if  (Correo.test(e.target.email)) {
-			alert("email valido");
-			console.log("correcto");
-		}
-		if ( (Correo.test(e.target.email)) === false ) {
-			console.log("error");
-		}
-		// if (textCamp.value.length > 20 ) {
-		// 	console.log("20")
-		// }
-	}
 
 
 	$(formulario).submit(function(e) {
@@ -37,7 +17,12 @@ $(document).ready(function () {
 			console.log("Email invalido");
 			return;
 		}
-
+		if (!(textCamp.length > 20)) {
+			console.log(textCamp.length)
+			console.log("Texto demasiado corto");
+			return;
+		}
+		
 		console.log("Formulario enviado")
 	})
 
