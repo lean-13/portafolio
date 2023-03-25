8d676cef1f7e0e187814b8134e4cd82d718c6a37
leandro <leandrodavid13@hotmail.com>
2023-03-25 14:18:21 -0300

    nuevas secciones

---
+++ js/Edad.js
@@ -0,0 +1,19 @@
+
+const EdadText = document.querySelector('.Edad');
+const Fecha = '20/10/2001';
+
+
+function calcularEdad(birthday) { 
+
+    birthday=new Date(birthday.split('/').reverse().join('-'));
+    var ageDifMs = Date.now() - birthday.getTime();
+    var ageDate = new Date(ageDifMs); 
+    
+    return Math.abs(ageDate.getUTCFullYear() - 1970);
+}
+
+
+console.log(calcularEdad(Fecha));
+
+EdadText.textContent = calcularEdad(Fecha);
+
